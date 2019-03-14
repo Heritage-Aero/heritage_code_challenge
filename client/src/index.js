7d92878b8d6f8ad2287135a0fddde09d68d9de61
3pwd <3pwd@tuta.io>
2019-03-14 01:30:29 +0100

    Setup store

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,25 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// import drizzle functions and contract artifact
+import { Drizzle, generateStore } from "drizzle";
+import LibraryApp from "./contracts/LibraryApp.json";
+
+// let drizzle know what contracts we want and how to access our test blockchain
+const options = {
+  contracts: [LibraryApp],
+  web3: {
+    fallback: {
+      type: "ws",
+      url: "ws://127.0.0.1:9545",
+    },
+  },
+};
+
+// setup the drizzle store and drizzle
+const drizzle = new Drizzle(options);
+
+ReactDOM.render(<App drizzle={drizzle}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
